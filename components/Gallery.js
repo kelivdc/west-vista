1592bd715c611573d6c3b86f2331a4ce06950662
Arie Ardiansyah <kelivdc@yahoo.com>
2022-10-14 21:52:42 +0700

    Image width

--- components/Gallery.js
+++ components/Gallery.js
@@ -27,7 +27,7 @@ function Gallery() {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    width: "100%",
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
